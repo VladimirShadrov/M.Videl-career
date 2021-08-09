927eff58a48a4588714bdd3aefb9891bb20f3a33
Vladimir <shadrov_vladimir@mail.ru>
2021-08-09 10:23:05 +0300

    Start project

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import '../styles/styles.scss';
+
+// Перенос изображений
+// require.context('../images', true, /\.(png|jpg|svg|gif)$/);
+// require.context('../fonts', true, /\.(ttf|woff|woff2)$/);
