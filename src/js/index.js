5626a001caaa5b9fd52b930c22dc1ef00092a401
Vladimir <shadrov_vladimir@mail.ru>
2021-08-12 22:44:18 +0300

    added: styles for advantages slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../styles/styles.scss';
 import '../styles/header.scss';
 import '../styles/createFuture.scss';
+import '../styles/advantages.scss';
 
 // Перенос изображений
 require.context('../images', true, /\.(png|jpg|svg|gif)$/);
