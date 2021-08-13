d8c4a8746fd163535af1e6bdd5a215d4ac8ee8ef
Vladimir <shadrov_vladimir@mail.ru>
2021-08-13 21:45:49 +0300

    added: styles for section vacancy

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import '../styles/header.scss';
 import '../styles/createFuture.scss';
 import '../styles/advantages.scss';
 import '../styles/goal.scss';
+import '../styles/profession.scss';
 
 // Перенос изображений
 require.context('../images', true, /\.(png|jpg|svg|gif)$/);
