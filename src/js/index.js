09401424073bf8188d4bc7c927d93d0967cedc26
Vladimir <shadrov_vladimir@mail.ru>
2021-11-19 17:41:47 +0300

    добавлена верстка блоков награды, бегущая строка, новости-события

--- src/js/index.js
+++ src/js/index.js
@@ -29,6 +29,9 @@ import '../styles/main-page/main-page-stage.scss';
 import '../styles/main-page/main-division.scss';
 import '../styles/main-page/main-mission.scss';
 import '../styles/main-page/main-video.scss';
+import '../styles/main-page/main-awards.scss';
+import '../styles/main-page/main-running-line.scss';
+import '../styles/main-page/main-news.scss';
 
 import { Vacancy } from './components/itHubPage/vacancy';
 import { Listing } from './components/vacancyListingPage/listing';
