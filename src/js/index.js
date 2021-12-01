6f5d4f1aabe27941647a8f8102e1cb6df960b3d0
Vladimir <shadrov_vladimir@mail.ru>
2021-12-01 16:26:00 +0300

    Add layout advantages block on page service and logistic

--- src/js/index.js
+++ src/js/index.js
@@ -35,6 +35,7 @@ import '../styles/main-page/main-news.scss';
 import '../styles/main-page/main-sustainability.scss';
 import '../styles/main-page/main-stability.scss';
 import '../styles/logistic/logistic-stage.scss';
+import '../styles/logistic/logistic-advantages.scss';
 
 import { Vacancy } from './components/itHubPage/vacancy';
 import { Listing } from './components/vacancyListingPage/listing';
