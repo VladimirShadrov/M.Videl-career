77f457e501799648ae2d4a66ddb4184c79508007
Vladimir <shadrov_vladimir@mail.ru>
2021-12-03 17:46:59 +0300

    add layout advantages block

--- src/js/index.js
+++ src/js/index.js
@@ -38,6 +38,7 @@ import '../styles/logistic/logistic-stage.scss';
 import '../styles/logistic/logistic-advantages.scss';
 import '../styles/logistic/logistic-career.scss';
 import '../styles/logistic/logistic-vacancy-directions.scss';
+import '../styles/logistic/logistic-why-we-are.scss';
 
 import { Vacancy } from './components/itHubPage/vacancy';
 import { Listing } from './components/vacancyListingPage/listing';
